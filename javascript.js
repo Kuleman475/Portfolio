d48a48f5b7f99c4248e91879a46fc999af8bff15
Use your GitHub username <Enter your email address>
2023-05-09 21:57:01 -0400

    slideshow 3 works

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
+let slideIndex = [1, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"];
 showSlides(1, 0);
 showSlides(1, 1);
-
+showSlides(1, 2);
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
@@ -12,14 +12,12 @@ function showSlides(n, no) {
   let x = document.getElementsByClassName(slideId[no]);
   
   if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
+  if (n < 2) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
-
-
 
 let slideshowIndex = 0;
 showTimedSlides();
@@ -38,5 +36,5 @@ function showTimedSlides() {
   }
   slides1[slideshowIndex-1].style.display = "block"; 
   dots[slideshowIndex-1].className += " active";
-  setTimeout(showTimedSlides, 2000); // Change image every 2 seconds
+  setTimeout(showTimedSlides, 4000); // Change image every 4 seconds
 }
