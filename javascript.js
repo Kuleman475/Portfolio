0508ed6356c7600879cd29d2f06be1516acc8b83
Use your GitHub username <Enter your email address>
2023-05-10 13:43:46 -0400

    slideshow done

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-let slideIndex = [1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3"];
+let slideIndex = [0, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
+showSlides(1, 3);
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
@@ -18,6 +19,7 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";  
 }
+
 
 let slideshowIndex = 0;
 showTimedSlides();
